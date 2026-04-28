fed0f40f47609d827f3b8dcb704d86cb65330a46
hlzvadik <golcovv716@gmail.com>
2026-04-28 19:15:17 +0300

    Update pursing

--- cpp-core/include/mylib/calculator/calculator.hpp
+++ cpp-core/include/mylib/calculator/calculator.hpp
@@ -8,7 +8,9 @@ namespace goltsov
   long long int priority(std::string a);
   goltsov::Queue< std::string > converToPostfix(goltsov::Queue< std::string > a);
   long long int convertStringToLLI(std::string a);
-  long long int eval(goltsov::Queue< std::string > postfix);
+  long long int solve(goltsov::Queue< std::string > postfix);
+  goltsov::Queue< std::string > parsing(std::string);
+  long long int eval(goltsov::Queue< std::string > inf);
 }
 
 #endif
